103f35b5126b2e57be3e1ea5f9a8af7631f7e386
SameBrain-Chris <77791115+SameBrain-Chris@users.noreply.github.com>
2022-11-09 16:48:59 -0500

    Tutorial Section 2 _ Adding Menu

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
 
 const Root = ({ state }) => {
   return (
     <>
       <h1>Frontity Workshop</h1>
         <p>Current URL: {state.router.link}</p>
+        <nav>
+          <Link link="/">Home</Link>
+          <Link link="/page/2">More Posts</Link>
+          <Link link="/about-us">About Us</Link>
+        </nav>
     </>
   )
 }
